d652e049804f4c82b6a77d512625f681b3a4af9f
Disha Agarwalla <dishaagarwalla260@gmail.com>
2025-12-22 10:10:24 +0530

    Add files via upload

---
+++ rec1.c
@@ -0,0 +1,21 @@
+int fact(int n){
+if(n==1)
+{
+    return 1;
+}
+else{
+    return n*fact(n-1); 
+}
+}
+
+void main()
+{
+    int r,num;
+printf("Enter a number: ");
+scanf("%d",&num);
+
+r=fact(num);
+printf("print factor is %d",r);
+
+getch();
+}
