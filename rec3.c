dc4cfbe431a3f87f6a818deb140ad38c5904b111
Disha Agarwalla <dishaagarwalla260@gmail.com>
2025-12-23 09:52:08 +0530

    Add files via upload

---
+++ rec3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+int linearSearch(int arr[], int size, int key) {
+    if (size == 0) {
+        return -1; 
+    }
+    if (arr[size - 1] == key) {
+        return size - 1; 
+    }
+    return linearSearch(arr, size - 1, key); 
+}
+
+int main() {
+    int n, key, i;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printf("Enter the key to search: ");
+    scanf("%d", &key);
+
+    
+    int result = linearSearch(arr, n, key);
+
+    
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found in the array\n");
+    }
+
+    return 0; 
+}
